22addab2ea36858c5252c8ce0640c16f47de129f
Сергей Кононов/Sergei Kononov <gem4ugov@icloud.com>
2023-05-20 08:04:31 +0300

    Update Sql_query.sql

--- Case Study #1 - Danny's Diner/Sql_query.sql
+++ Case Study #1 - Danny's Diner/Sql_query.sql
@@ -1,9 +1,16 @@
 --1.What is the total amount each customer spent at the restaurant?
 
 SELECT
-	customer_id,
+	customer_id AS customer,
 	SUM(price) AS total_amount
 FROM sales
 JOIN menu USING(product_id)
 GROUP BY customer_id
 ORDER BY total_amount DESC
+
+customer|total_amount|
+--------+------------+
+A       |          76|
+B       |          74|
+C       |          36|
+
